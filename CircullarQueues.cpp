a8232000a3b06bf7d4845321ec71ef70e210b60a
Nafa06 <naufalanggardi09@gmail.com>
2025-06-12 11:23:47 +0700

    Memperbaiki menu

--- CircullarQueues.cpp
+++ CircullarQueues.cpp
@@ -127,8 +127,8 @@ int main()
             cout << "Menu" << endl;
             cout << "1. Implement insert operation" << endl;
             cout << "2. Implement delete operation" << endl;
-            cout << "Display values" << endl;
-            cout << "Exit" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
             cout << "Enter your choice (1-4): ";
             cin >> ch;
             cout << endl;
