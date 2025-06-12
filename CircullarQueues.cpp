74b2c52a812a459d8502dd56b8e7d03f1fbdbbbb
Nafa06 <naufalanggardi09@gmail.com>
2025-06-12 10:29:09 +0700

    Menambahkan tahap kedua untuk memeriksa apakah antrian kosong

--- CircullarQueues.cpp
+++ CircullarQueues.cpp
@@ -27,5 +27,21 @@ public:
             cout << "\nQueue overflow\n"; // 1.a
             return;                       // 1.b
         }
+        
+        // 2. cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0; // 2.a
+            REAR = 0;  // 2.b
+        }
+        else
+        {
+            // Jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 };
